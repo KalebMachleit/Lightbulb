6c58ffd4745b799fd1c15698464508a45d71ebcc
KalebMachleit <machleit0225@gmail.com>
2024-06-25 22:00:12 -0700

    fixed background

--- JS/script.js
+++ JS/script.js
@@ -7,14 +7,14 @@ function lightSwitch() {
         document.getElementById("offBulb").style.display = "none"
         document.getElementById("onBulb").style.display = "block"
         // document.getElementById("switch").style.rotate = "180deg"
-        document.getElementById("container").style.backgroundColor = "beige"
+        document.body.style.backgroundColor = "beige"
         r.style.setProperty('--text', 'black')
         isOn = true
     } else {
         document.getElementById("onBulb").style.display = "none"
         document.getElementById("offBulb").style.display = "block"
         // document.getElementById("switch").style.rotate = "0deg"
-        document.getElementById("container").style.backgroundColor = "gray"
+        document.body.style.backgroundColor = "gray"
         r.style.setProperty('--text', 'white')
         isOn = false
     }
